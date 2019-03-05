c96f5409f8f9668e533359f489e3a209d2f573fc
Aamod Krishna Tiwari <aamod.tiwari_ccv17@gla.ac.in>
2019-03-05 14:56:28 +0530

    some changes in the code

--- src/doubly/DoublyLinkedList.java
+++ src/doubly/DoublyLinkedList.java
@@ -1,44 +1,77 @@
 package doubly;
 
 public class DoublyLinkedList {
-    private static class Node{
+    private static class Node {
         private int data;
         private Node pre;
         private Node next;
 
-        public Node(int data, Node pre, Node next){
+        public Node(int data, Node pre, Node next) {
             this.data = data;
             this.pre = pre;
             this.next = next;
         }
-        public Node(int data){
-            this.data=data;
+
+        public Node(int data) {
+            this.data = data;
             this.pre = null;
             this.next = null;
         }
-           private int size = 0;
-           private Node head = null;
-           private Node tail = null;
+
+        private int size = 0;
+        private Node head = null;
+        private Node tail = null;
 
         public static void main(String[] args) {
             DoublyLinkedList linkedList = new DoublyLinkedList();
             System.out.println(linkedList);
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder response = new StringBuilder();
-            response.append("[");
-            Node temp = this.head;
-            while(temp!= null){
-                response.append(temp.data);
-                temp = temp.next;
+            for (int i = 0; i < 5; i++) {
+                linkedList.insert(i + 1);
             }
-            response.append("]");
-            return response.toString();
-        }
-        public void insert(int data){
-
+            System.out.println(linkedList);
         }
     }
+
+
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
+        response.append("[");
+        Node temp = this.head;
+        while (temp != null) {
+            response.append(temp.data);
+            if (temp.next != null) {
+                response.append(" <==> ");
+            }
+            temp = temp.next;
+        }
+        response.append("]");
+        return response.toString();
+    }
+
+    public void insertHead(int data) {
+             /* Node newNode = new Node(data,null,this.head);
+            this.head = newNode;*/
+        this.head = new Node(data, null, this.head);
+        size++;
+    }
+
+    private vois insertAfter(int data, Node node) {
+        Node newNode = new Node(data, node, node.next );
+        node.next= newNode;
+        newNode.next.pre = newNode;
+        size++;
+    }
+      public void insert(int data){
+        if(head == null){
+            insertHead(data);
+        }
+        else{
+            Node temp = this.head;
+            while (temp!= null){
+                temp= temp.next;
+            }
+            insertAfter(data, temp);
+        }
+      }
 }
